43687c1240866cf27b3d39f77c8763b4754ecedd
clearplaid <clearplaid@gmail.com>
2019-04-20 09:45:17 -0400

    firebase

---
+++ javascript/game.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBZfnmHbF_SMT7DqIFh6Kdp3LGVV32YA_A",
+    authDomain: "awesometeamtimesheet.firebaseapp.com",
+    databaseURL: "https://awesometeamtimesheet.firebaseio.com",
+    projectId: "awesometeamtimesheet",
+    storageBucket: "awesometeamtimesheet.appspot.com",
+    messagingSenderId: "361987824664"
+  };
+  
+  firebase.initializeApp(config);
