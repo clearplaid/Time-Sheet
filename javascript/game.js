db0ee003232476275badef3347bb1ebcca7c44d0
clearplaid <clearplaid@gmail.com>
2019-04-20 10:30:52 -0400

    js updated

--- javascript/game.js
+++ javascript/game.js
@@ -7,5 +7,29 @@ var config = {
     storageBucket: "awesometeamtimesheet.appspot.com",
     messagingSenderId: "361987824664"
   };
-  
+
   firebase.initializeApp(config);
+  var database = firebase.database();
+
+
+  $('#add-employee').on("click", function(){
+    // get value
+    var name = $('name-input').val().trim();
+    var role = $('role-input').val().trim();
+    var startDate = $('date-input').val().trim();
+    var monthlyRate = $('rate-input').val().trim();
+    // validate
+    if (name === "" || role === "" || startDate === "" || monthlyRate === "") return;
+    // empty inputs
+
+    
+    // compute field data
+
+    // add to database
+    database.ref().push({name: name, 
+                        role: role, 
+                        startDate: startDate, 
+                        monthlyRate: monthlyRate, 
+                        dataAdded: firebase.database.ServerValue.TIMESTAMP});
+
+  });
